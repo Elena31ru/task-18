bfe3a42fad0979af38df280d5c27813741aae552
elena31ru <elena-31ru@yandex.ru>
2023-03-16 19:58:43 +0300

    init

---
+++ config.py
@@ -0,0 +1,12 @@
+import telebot
+
+
+TOKEN = '6115483176:AAFrRWNVb6nMGG1BbHAO5M9lmDMQrZ2A1RY'
+
+bot = telebot.TeleBot(TOKEN)
+
+CURRENCIES = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
